21028325be9c25d256612d37ea1cdfef99073d29
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-10-27 09:53:45 +0530

    changes

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Shape from "./Shape";
+import Circle from "./Circle";
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Shape />
+        <Circle />
+      </>
+    );
+  }
+}
+export default App;
